950d8ae09e01a88af51243b19287cd7e8821dce7
Fabricio Nogueira <fabricionpinheiro@alu.ufc.br>
2021-03-10 16:08:55 -0300

    first commit of module 1-backend

---
+++ implementations/init/CSharpStructureTutorial/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CSharpStructureTutorial
+{
+    public struct Employee {
+        public string empNumber;
+        public string empName;
+        public string position;
+
+        public Employee(string empNumber, string empName, string position) {
+            this.empNumber = empNumber;
+            this.empName = empName;
+            this.position = position;
+        }
+
+        /*
+        public Employee(string empNumber, string empName) {
+            this.empNumber = empNumber;
+            this.empName = empName;
+        }
+        */
+
+    }
+    class Program { 
+        static void Main(string[] args) {
+            Employee one = new Employee("7", "fabs", "1");
+            Console.WriteLine(one.empNumber);
+            Console.WriteLine(one.empName);
+            Console.WriteLine(one.position);
+            Employee two = one;
+            two.empNumber = "9";
+            Console.WriteLine(two.empName);
+
+        }
+    }
+}
---
+++ implementations/init/ConvertTo2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConvertTo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+            string str = "3456";
+            result = Convert.ToInt32(str);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
